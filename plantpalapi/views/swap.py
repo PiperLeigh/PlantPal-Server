4419362e616ad70b998d8f7837b2463f36e40416
Piper Leigh <pipergillianleigh@gmail.com>
2022-09-14 22:42:50 -0500

    Added create/update/delete to SwapView

--- plantpalapi/views/swap.py
+++ plantpalapi/views/swap.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from plantpalapi.models import Swap, PlantPalUser
+from plantpalapi.models import Swap, PlantPalUser, swap
 
 class SwapView(ViewSet):
     """Swap view - list of plant pal swaps
@@ -25,6 +25,53 @@ class SwapView(ViewSet):
         host = PlantPalUser.objects.get(user=request.auth.user)
         serializer = SwapSerializer(swaps, many=True)
         return Response(serializer.data)
+
+    def create(self, request):
+        """Handle POST operations
+
+        Returns
+            Response -- JSON serialized plant instance
+        """
+        #Getting the gamer that is logged in using their aut token
+        host = PlantPalUser.objects.get(user=request.auth.user)
+
+        swap = Swap.objects.create(
+            title=request.data["title"],
+            coverPhoto=request.data["coverPhoto"],
+            host=host,
+            location=request.data["location"],
+            date=request.data["date"],
+            time=request.data["time"],
+            description=request.data["description"]
+        )
+        serializer = SwapSerializer(swap)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, pk):
+        """Handle PUT requests for a swap
+
+        Returns:
+        Response -- Empty body with 204 status code
+        """
+        host = PlantPalUser.objects.get(user=request.auth.user)
+
+        swap = Swap.objects.get(pk=pk)
+        swap.title = request.data["title"]
+        swap.coverPhoto = request.data["coverPhoto"]
+        swap.host = host
+        swap.location = request.data["location"]
+        swap.date = request.data["date"]
+        swap.time = request.data["time"]
+        swap.description = request.data["description"]
+
+        swap.save()
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk):
+        swap = Swap.objects.get(pk=pk)
+        swap.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
 
 class SwapSerializer(serializers.ModelSerializer):
     """"JSON serializer for swaps
